19b748278f4cbd2c9da2462060694dbc21823f75
Ansh Arora <theboywholived@Anshs-MacBook-Air.local>
2021-08-19 19:35:48 +0530

    second commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,7 @@ const validator = require('validator')
 
 mongoose.connect('mongodb+srv://ansharora:vardy3999@cluster0.5og1b.mongodb.net/task-app?retryWrites=true&w=majority',{
     useNewUrlParser : true,
-    useCreateIndex : true
+    useCreateIndex : true,
+    useFindAndModify : false
 })
 
